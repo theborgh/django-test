06c1626c95aea090336ab9b5953427d22b1a4357
theborgh <borghiborghi@gmail.com>
2024-01-09 18:24:05 +0100

    add notes model

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # my apps
     'home',
+    'notes',
 ]
 
 MIDDLEWARE = [
---
+++ notes/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Notes(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
