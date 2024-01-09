27e46bfa8eecbda3dc7c410ccaaf3f9d0976111e
theborgh <borghiborghi@gmail.com>
2024-01-09 20:36:57 +0100

    add styles

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list', views.NotesListView.as_view()),
-    path('detail/<int:pk>', views.NotesDetailView.as_view()),
+    path('list', views.NotesListView.as_view(), name='notes.list'),
+    path('detail/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail'),
 ]
