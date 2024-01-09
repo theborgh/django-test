0d5617e7176e53c0e771f2cf03ac0c5703846c70
theborgh <borghiborghi@gmail.com>
2024-01-09 19:18:48 +0100

    add note details view

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
+    path('list', views.list),
+    path('detail/<int:note_id>', views.detail),
 ]
--- notes/views.py
+++ notes/views.py
@@ -5,3 +5,7 @@ from .models import Notes
 def list(request):
     notes = Notes.objects.all()
     return render(request, 'notes/list.html', {'notes': notes})
+
+def detail(request, note_id):
+    note = Notes.objects.get(id=note_id)
+    return render(request, 'notes/detail.html', {'note': note})
