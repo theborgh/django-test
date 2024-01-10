7ae91be8121753a6ac7aa496fb89173bb20419b8
theborgh <borghiborghi@gmail.com>
2024-01-10 10:15:42 +0100

    display notes by user

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Notes(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 
 from .models import Notes
 from .forms import NotesForm
@@ -9,10 +11,20 @@ class NotesCreateView(CreateView):
     form_class = NotesForm
     success_url = '/notes/list'
 
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     template_name = 'notes/list.html' # must add because default is notes/notes_list.html
     context_object_name = 'notes'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
